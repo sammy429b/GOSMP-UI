e896ccac4cc390a20a8d60e4f5edb1888d7e9879
asyncdoggo <ayushdeshpande81@gmail.com>
2024-02-24 11:04:54 +0530

    Add api call for optimize

--- src/pages/Optimize.jsx
+++ src/pages/Optimize.jsx
@@ -5,18 +5,74 @@ import { useState } from "react"
 export default function Optimize() {
 
     const [investmentAmount, setInvestmentAmount] = useState(100000)
-    const [duration, setDuration] = useState(0)
+    const [duration, setDuration] = useState(1)
     
     const [diversifyPortfolio, setDiversifyPortfolio] = useState(false)
 
+    const [sectors, setSectors] = useState([])
+
+    const [optimizedData, setOptimizedData] = useState({
+        loading: false,
+        data: null
+    })
+
     const getDateWithDuration = (duration) => { 
         const startDate = Date.now()
-        const endDate = new Date(startDate) + duration
+        const endDate = new Date(startDate) + duration * 30 * 24 * 60 * 60 * 1000
         const newEndDate = new Date(endDate)
         return newEndDate.toDateString()
     }
 
     const dialogRef = useRef()
+
+
+
+    const optimize = async () => { 
+        if (investmentAmount === 0) { 
+            alert("Please enter the amount you want to invest")
+            return
+        }
+        if (duration === 0) { 
+            alert("Please select a duration to invest")
+            return
+        }
+        if (diversifyPortfolio) { 
+            // set sectors
+        }
+        setOptimizedData({
+            ...optimizedData,
+            loading: true
+        })
+
+        await fetch("http://localhost:8000/optimize", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                risk_category: "Low risk",
+                risk_score: 0.4,
+                invest_amount: investmentAmount,
+                duration: duration * 30
+            })
+        }).then(response => response.json())
+            .then(data => {
+                setOptimizedData({
+                    loading: false,
+                    data: data
+                })
+            })
+            .catch(error => { 
+                console.error(error)
+                setOptimizedData({
+                    loading: false,
+                    data: null
+                })
+            })
+    }
+
+
+
 
     useEffect(() => { 
         const question = document.querySelector("#question")
@@ -38,20 +94,169 @@ export default function Optimize() {
                 span.hidden = true
             })
         }
-    },[])
+    }, [])
+    
 
+    const fullData = {
+        "equal_weights_results": {
+            "portfolio_variance": 0.0049602973625222264,
+            "portfolio_volatility": 0.07042937854703978,
+            "portfolio_annual_return": 0.09609689566821158,
+            "percent_var": "0.0%",
+            "percent_vols": "7.000000000000001%",
+            "percent_ret": "10.0%"
+        },
+        "optimized_results": {
+           "performance": {
+                "expected_returns": 0.7088986649000859,
+                "volatility": 0.10000000019994155,
+                "sharpe_ratio": 6.888986635226912
+            },
+            "weights": {
+                "360ONE": 2.2310446208924177,
+                "BAJAJ-AUTO": 11.591231824636491,
+                "BRITANNIA": 4.775095501910038,
+                "CIPLA": 5.439108782175643,
+                "DATAPATTNS": 0.10900218004360088,
+                "DRREDDY": 1.9420388407768152,
+                "EPIGRAL": 0.21000420008400164,
+                "GILLETTE": 1.5930318606372125,
+                "GLAXO": 4.1340826816536325,
+                "MEDANTA": 10.783215664313285,
+                "HAL": 1.7410348206964137,
+                "IRFC": 4.163083261665233,
+                "KALYANKJIL": 5.4881097621952435,
+                "KAYNES": 5.581111622232444,
+                "KFINTECH": 2.2800456009120182,
+                "LT": 1.1860237204744095,
+                "MRF": 8.435168703374066,
+                "MANKIND": 0.08200164003280064,
+                "MAXHEALTH": 4.123082461649233,
+                "METROBRAND": 1.8290365807316145,
+                "NESTLEIND": 2.772055441108822,
+                "PFIZER": 3.005060101202024,
+                "POLYCAB": 1.6590331806636134,
+                "POWERGRID": 2.318046360927218,
+                "RAINBOW": 3.932078641572831,
+                "SANOFI": 4.835096701934038,
+                "TCS": 2.470049400988019,
+                "VIJAYA": 1.2920258405168101
+            },
+            "invested": {
+                "360ONE": {
+                    "price": 713.7000122070312,
+                    "units": 3,
+                    "allocated": 2141.1000366210938
+                },
+                "BAJAJ-AUTO": {
+                    "price": 8232.25,
+                    "units": 1,
+                    "allocated": 8232.25
+                },
+                "BRITANNIA": {
+                    "price": 4923,
+                    "units": 1,
+                    "allocated": 4923
+                },
+                "CIPLA": {
+                    "price": 1447.300048828125,
+                    "units": 4,
+                    "allocated": 5789.2001953125
+                },
+                "GLAXO": {
+                    "price": 2238.35009765625,
+                    "units": 2,
+                    "allocated": 4476.7001953125
+                },
+                "MEDANTA": {
+                    "price": 1432.25,
+                    "units": 8,
+                    "allocated": 11458
+                },
+                "HAL": {
+                    "price": 2948.050048828125,
+                    "units": 1,
+                    "allocated": 2948.050048828125
+                },
+                "IRFC": {
+                    "price": 154.1999969482422,
+                    "units": 27,
+                    "allocated": 4163.399917602539
+                },
+                "KALYANKJIL": {
+                    "price": 376.25,
+                    "units": 15,
+                    "allocated": 5643.75
+                },
+                "KAYNES": {
+                    "price": 2811.050048828125,
+                    "units": 2,
+                    "allocated": 5622.10009765625
+                },
+                "KFINTECH": {
+                    "price": 651,
+                    "units": 4,
+                    "allocated": 2604
+                },
+                "MAXHEALTH": {
+                    "price": 854.0499877929688,
+                    "units": 5,
+                    "allocated": 4270.249938964844
+                },
+                "METROBRAND": {
+                    "price": 1120.550048828125,
+                    "units": 2,
+                    "allocated": 2241.10009765625
+                },
+                "NESTLEIND": {
+                    "price": 2543.60009765625,
+                    "units": 1,
+                    "allocated": 2543.60009765625
+                },
+                "PFIZER": {
+                    "price": 4510.5498046875,
+                    "units": 1,
+                    "allocated": 4510.5498046875
+                },
+                "POWERGRID": {
+                    "price": 280.1000061035156,
+                    "units": 8,
+                    "allocated": 2240.800048828125
+                },
+                "RAINBOW": {
+                    "price": 1275.0999755859375,
+                    "units": 3,
+                    "allocated": 3825.2999267578125
+                },
+                "SANOFI": {
+                    "price": 9197.4501953125,
+                    "units": 1,
+                    "allocated": 9197.4501953125
+                },
+                "TCS": {
+                    "price": 3991.5,
+                    "units": 1,
+                    "allocated": 3991.5
+                },
+                "VIJAYA": {
+                    "price": 645,
+                    "units": 2,
+                    "allocated": 1290
+                }
+            },
+            "remaining": 7887.899301696918
+        }
+    }
+    
     return (
         <>
             <div className="w-full h-full flex flex-col justify-center items-center">
 
                 <h1 className="text-4xl font-semibold mt-24">Portfolio Optimization</h1>
 
-                <div className="w-[40%]">
-                    
-
-                    
-                    
-                    <div className="my-1">
+                <div className="w-full flex flex-col items-center">
+                  
+                    <div className="my-1 w-1/2 flex justify-center">
                         <label className="form-control w-full max-w-lg">
                             <div className="label">
                                 <span className="label-text text-base">Enter the amount you want to invest</span>
@@ -60,7 +265,7 @@ export default function Optimize() {
                         </label>
                     </div>
 
-                    <div className="my-1">
+                    <div className="my-1 w-1/2 self-center flex flex-col items-center">
                         <label className="form-control w-full max-w-lg">
                             <div className="label">
                                 <span className="label-text text-base">Select duration to invest</span>
@@ -73,19 +278,16 @@ export default function Optimize() {
                                 <option value={7}>18 months</option>
                                 <option value={8}>24 months</option>
                             </select>
-                        </label>
-                    </div>
-
-
-                    <div className="my-1 pl-2 italic text-sm">
-                        <p>
+                        <p className="italic text-sm self-start">
                             {
                                 duration === 0 ? "Select a duration to invest" : `duration end date is ${getDateWithDuration(duration)}`
                             }
                         </p>
+                        </label>
+
                     </div>
 
-                    <div className="my-1">
+                    <div className="my-1 w-1/2 flex justify-center">
                         <div className="form-control w-full max-w-lg">
                             <div className="mt-2 flex flex-row justify-start">
                                 <label htmlFor="diversify" className="cursor-pointer label">
@@ -134,11 +336,106 @@ export default function Optimize() {
                         )
                     }
 
-                    <div>
-                        <button className="btn btn-neutral">
+                    <div className="my-1 mb-8 w-1/2 flex justify-center">
+                        <div className="padding-class form-control w-full max-w-lg">
+                        <button className="btn btn-neutral w-32" onClick={optimize}>
                             Optimize
                         </button>
+                        </div>
                     </div>
+
+                    {
+                        optimizedData.loading && (
+                            <div className="my-2 w-full flex justify-center items-center">
+                                <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12">
+                                    
+                                </div>
+                            </div>
+                        )
+                    }
+
+                    {
+                        !optimizedData.data && (
+                            <>
+                                {/* Fill fullData */}
+                                <div className="my-2 w-2/3">
+                                    <p className="text-4xl font-semibold">Optimized Results</p>
+
+                                    <span className="divider"></span>
+
+                                    <div className="flex flex-col gap-y-2">
+                                        <p className="text-2xl">Equal weights results</p>
+                                        <div className="flex flex-row gap-x-2">
+                                            <span>Portfolio variance</span>
+                                            <span>{(fullData.equal_weights_results.portfolio_variance * 100).toFixed(2)}%</span>
+                                        </div>
+                                        <div className="flex flex-row gap-x-2">
+                                            <span>Portfolio volatility</span>
+                                            <span>{(fullData.equal_weights_results.portfolio_volatility * 100).toFixed(2)}%</span>
+                                        </div>
+                                        <div className="flex flex-row gap-x-2">
+                                            <span>Portfolio annual return</span>
+                                            <span>{(fullData.equal_weights_results.portfolio_annual_return * 100).toFixed(2)}%</span>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="my-2 w-2/3">
+                                    <h2 className="text-2xl font-semibold">Optimized Results</h2>
+                                    <div className="flex flex-col gap-y-2">
+                                        {
+                                            <div className="flex flex-col gap-y-2">
+                                                <div className="flex flex-row gap-x-2">
+                                                    <span>Expected returns</span>
+                                                    <span>{(fullData.optimized_results.performance.expected_returns * 100).toFixed(2)}%</span>
+                                                </div>
+                                                <div className="flex flex-row gap-x-2">
+                                                    <span>Volatility</span>
+                                                    <span>{(fullData.optimized_results.performance.volatility * 100).toFixed(2)}%</span>
+                                                </div>
+                                                <div className="flex flex-row gap-x-2">
+                                                    <span>Sharpe ratio</span>
+                                                    <span>{fullData.optimized_results.performance.sharpe_ratio}</span>
+                                                </div>
+                                            </div>
+                                        }                                           
+                                    </div>
+                                </div>
+
+                                {/* Insert an editable table with invested and weights */}
+                                <div className="my-2 w-2/3">
+                                    <h2 className="text-xl font-semibold">Invested</h2>
+                                    <table className="table w-full">
+                                        <thead>
+                                            <tr>
+                                                <th>Stock</th>
+                                                <th>Price</th>
+                                                <th>Units</th>
+                                                <th>Allocated</th>
+                                                <th>Percentage</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {
+                                                Object.keys(fullData.optimized_results.invested).map((key, index) => {
+                                                    return (
+                                                        <tr key={index}>
+                                                            <td>{key}</td>
+                                                            <td>{fullData.optimized_results.invested[key].price.toFixed(2)}</td>
+                                                            <td>{fullData.optimized_results.invested[key].units}</td>
+                                                            <td>{fullData.optimized_results.invested[key].allocated.toFixed(2)}</td>
+                                                            <td>{(fullData.optimized_results.weights[key]).toFixed(2)}%</td>
+                                                        </tr>
+                                                    )
+                                                })
+                                            }
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </>
+                        )
+                    }
+
                 </div>
             </div>
         </>
