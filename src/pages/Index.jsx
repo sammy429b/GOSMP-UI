993b20e36fc9a58486144080d5eebc8bb21d5134
sammy429b <samarthnbahirgonde49@gmail.com>
2024-03-04 15:22:11 +0530

    landing page roadmap added

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,19 +1,30 @@
 import React from 'react'
-import img from "../assets/trading.jpg"
 import img1 from "../assets/logo.jpg"
+import {Link} from "react-router-dom"
+import Timeline from '../components/Timeline'
 const Index = () => {
   return (
     <>
         <div className='w-full flex flex-row h-full'>
-          <div className='w-[50%] flex flex-col justify-center gap-y-2 items-start px-8'>
-            <h1 className='text-[4rem] font-semibold'>
-              Your Fiancial Status Is Our Goal
-            </h1>
-            <button className="btn btn-primary text-lg ">Get Started</button>
+          <div className='w-[50%] flex flex-col justify-center items-start px-10'>
+            <h1 className='text-xl font-medium ml-1 text-accent '>Welcome to GOSMP</h1>
+            <h2 className='text-[3.8rem] font-semibold leading-snug'>
+              Get More From Your Finance
+            </h2>
+            <button className="btn btn-primary text-lg my-3">
+              <Link to="/riskForm">
+              Get Started
+              </Link>
+              </button>
           </div>
           <div className='w-[50%]  flex justify-center items-center'>
-            <img src={img1} alt="" className='bg-blend-color'/>
+            <img src={img1} alt="" className='bg-blend-color mt-4'/>
           </div>
+        </div>
+
+        <div className='mx-48 flex flex-col gap-y-2 mt-20'>
+          <h2 className='text-2xl font-semibold text-center '>How it Works</h2>
+          <Timeline/>
         </div>
     </>
   )
