661c3a42267d0eec58960436b7f2dc1e5c69ab7f
Shubham Chougule <89000326+CosmicStrike@users.noreply.github.com>
2024-03-04 12:55:27 +0530

    Revert "add hero"
    This reverts commit e3164cec35f9b79c45c906da48503632eac3ce35.

--- src/App.jsx
+++ src/App.jsx
@@ -5,32 +5,32 @@ import Optimize from './pages/Optimize'
 import App_Layout from './layout/layout'
 import Index from './pages/Index'
 
-
+  
 const router = createBrowserRouter([
   {
-    element: <App_Layout />,
-    children: [
-      {
-        path: '/',
-        element: <Index />
-      },
-      {
-        path: '/riskform',
-        element: <UserForm />
-      },
-      {
-        path: '/optimize',
-        element: <Optimize />
-      }
-    ]
-  }]
+    element:<App_Layout/>,
+  children: [
+  {
+    path: '/',
+    element: <Index/>
+  },
+  {
+    path: '/riskform',
+    element: <UserForm/>
+  },
+  {
+    path: '/optimize',
+    element: <Optimize/>
+  }
+]
+}]
 )
 
 
 
 function App() {
   return (
-    <RouterProvider router={router} />
+    <RouterProvider router={router}/>
   )
 }
 
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,20 +1,8 @@
 import React from 'react'
-import hero from '/public/hero.jpg'
+
 const Index = () => {
   return (
     <>
-      <div className='flex'>
-        <img src={hero} className='relative w-[100%] h-[38rem]' />
-        <div className='ml-4 absolute text-white my-24'>
-          <h1 className='font-bold text-6xl'>Finiancial Portfolios</h1>
-          <h1 className='font-bold text-3xl'>Invest in future trend</h1>
-          <h1 className='font-bold text-xl'>BE RICH, BE COOL</h1>
-          <button className='w-32 h-12 my-10 bg-blue-600 text-white font-bold text-lg'>Get Started</button>
-        </div>
-      </div>
-      <section>
-        <h1></h1>
-      </section>
 
     </>
   )
