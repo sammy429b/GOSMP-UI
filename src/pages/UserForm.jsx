872a272a886225759ca58d7eec2320a02fd14836
asyncdoggo <ayushdeshpande81@gmail.com>
2024-03-05 09:09:55 +0530

    Add pie chart

--- src/pages/UserForm.jsx
+++ src/pages/UserForm.jsx
@@ -89,7 +89,7 @@ export default function UserForm() {
                 </div>
 
 
-                <button to="/optimize" className="my-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-3 " onClick={optimizePage}>Next ></button>
+                <button to="/optimize" className="my-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-3 " onClick={optimizePage}>Next</button>
             </div>
         </>
     )
